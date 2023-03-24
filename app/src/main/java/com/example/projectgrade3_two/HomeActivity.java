a6ccab9ea7682ddf7ad0c4d2862a92061c39c1c9
LAPTOP-UF2JJMM9\chiu9 <tyuighjk86@gmail.com>
2023-03-24 18:24:32 +0800

    home的XML先加要出現的資料名

--- app/src/main/java/com/example/projectgrade3_two/HomeActivity.java
+++ app/src/main/java/com/example/projectgrade3_two/HomeActivity.java
@@ -1,17 +1,27 @@
 package com.example.projectgrade3_two;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Button btn_logout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +37,7 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(new Intent(HomeActivity.this, MainActivity.class));
             }
         });
+
+
     }
 }
