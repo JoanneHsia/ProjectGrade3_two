ae3fed2aeb8b972632d2754e708d2e55083058f0
CGH200\cgh200 <nicoyazawa0501@gmail.com>
2023-03-27 16:34:15 +0800

    其他頁也隱藏起來

--- app/src/main/java/com/example/projectgrade3_two/MainActivity.java
+++ app/src/main/java/com/example/projectgrade3_two/MainActivity.java
@@ -27,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        getWindow().getDecorView().setSystemUiVisibility(
+
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION //隱藏狀態欄和標題欄
+
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION //全螢幕顯示
+
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY); //隱藏手機虛擬按鍵HOME/BACK/LIST按鍵
+
+
         btn_login = (Button) findViewById(R.id.btn_login);
 
         emailText = (EditText) findViewById(R.id.tx_email);
--- app/src/main/java/com/example/projectgrade3_two/WelcomeActivity.java
+++ app/src/main/java/com/example/projectgrade3_two/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.example.projectgrade3_two;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,16 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION //隱藏狀態欄和標題欄
+
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION //全螢幕顯示
+
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY); //隱藏手機虛擬按鍵HOME/BACK/LIST按鍵
+
+
         mHandler.sendEmptyMessageDelayed(GOTO_MAIN_ACTIVITY, 1500);
     }
 
