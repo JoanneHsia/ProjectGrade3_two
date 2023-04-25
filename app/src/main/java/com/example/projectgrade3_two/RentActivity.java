8d49429ee3bea0bc7205b0b723daf6e5c4e246c4
LAPTOP-UF2JJMM9\chiu9 <tyuighjk86@gmail.com>
2023-04-25 19:52:01 +0800

    借入/借出掃qrcode

--- app/src/main/java/com/example/projectgrade3_two/RentActivity.java
+++ app/src/main/java/com/example/projectgrade3_two/RentActivity.java
@@ -7,12 +7,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class RentActivity extends AppCompatActivity {
 
     private Activity context=this;
+
+    Button btn_lend, btn_borr;
+    String item_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +34,57 @@ public class RentActivity extends AppCompatActivity {
             }
         });
 
+        btn_lend = findViewById(R.id.btn_lend);
+        btn_borr = findViewById(R.id.btn_borr);
+
+        btn_lend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator=new IntentIntegrator(RentActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setCameraId(0);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.setOrientationLocked(true);
+                integrator.initiateScan();
+            }
+        });
+
+        btn_borr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator=new IntentIntegrator(RentActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setCameraId(0);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.setOrientationLocked(true);
+                integrator.initiateScan();
+            }
+        });
 
 
+
+
+
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult ScanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode,data);
+
+        if(ScanResult !=null){
+            if(ScanResult.getContents() != null) {
+                String Scan = ScanResult.getContents();
+                if(!Scan.equals("")){
+                    item_id = Scan.trim();
+                    Intent intent = new Intent(RentActivity.this, DoneActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("item_id", item_id);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }
+            }
+        }else{
+            super.onActivityResult(requestCode,resultCode,data);
+            Toast.makeText(RentActivity.this, "請重新掃描", Toast.LENGTH_SHORT).show();
+        }
     }
 }
