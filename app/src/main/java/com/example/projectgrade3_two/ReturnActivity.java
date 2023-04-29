207322bf2d3e20f9e0c81a5ab9705cc1bb9b77e0
LAPTOP-UF2JJMM9\chiu9 <tyuighjk86@gmail.com>
2023-04-29 12:55:12 +0800

    更新歸還功能

--- app/src/main/java/com/example/projectgrade3_two/ReturnActivity.java
+++ app/src/main/java/com/example/projectgrade3_two/ReturnActivity.java
@@ -7,12 +7,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReturnActivity extends AppCompatActivity {
 
+    String item_id, user_id;
+
+    Button btn_retSend;
+    String urlRetUpdate = "https://projectgrade3two.000webhostapp.com/updateBed.php";
+    String urlitemid = "https://projectgrade3two.000webhostapp.com/searchItem.php";
+
+    TextView txtItemID, txtItemName, txtItemStatus;
+
     private Activity context=this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +45,6 @@ public class ReturnActivity extends AppCompatActivity {
         setContentView(R.layout.activity_return);
 
         Button backPageBtn = (Button)findViewById(R.id.return_btn);
-        Button btn_returnScan = (Button)findViewById(R.id.btn_returnScan);
         backPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,17 +54,132 @@ public class ReturnActivity extends AppCompatActivity {
             }
         });
 
-        btn_returnScan.setOnClickListener(new View.OnClickListener() {
+
+        btn_retSend = findViewById(R.id.btn_return_send);
+
+        Bundle bundle =  getIntent().getExtras();
+        item_id = bundle.getString("item_id");
+        user_id = bundle.getString(("user_id"));
+
+        itemQrcode(item_id);
+
+        btn_retSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                IntentIntegrator integrator=new IntentIntegrator(context);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                integrator.setCameraId(0);
-                integrator.setBarcodeImageEnabled(false);
-                integrator.setOrientationLocked(true);
-                integrator.initiateScan();
+                updateRet(item_id);
+                Intent intent = new Intent(ReturnActivity.this, HomeActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("user_id",user_id);
+                intent.putExtras(bundle);
+                startActivity(intent);
             }
         });
+
+
+    }
+    private void itemQrcode(String item_id){
+
+        StringRequest request = new StringRequest(Request.Method.POST, urlitemid,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("itemProfile");
+
+                            if (success.equals("1")) {
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject object = jsonArray.getJSONObject(i);
+
+                                    String itemId = object.getString("item_id").trim();
+                                    String itemName = object.getString("item_name").trim();
+                                    String itemStatus = object.getString("item_status").trim();
+
+                                    txtItemID = findViewById(R.id.txt_retItemID);
+                                    txtItemName = findViewById(R.id.txt_retItemName);
+                                    txtItemStatus = findViewById(R.id.txt_retItemStatus);
+
+                                    txtItemID.setText(itemId);
+                                    txtItemName.setText(itemName);
+                                    txtItemStatus.setText(itemStatus);
+
+//                                    Toast.makeText(DoneActivity.this, "ok4", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(ReturnActivity.this, "err", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ReturnActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("item_id", item_id);
+
+                return params;
+            }
+        };
+
+        Volley.newRequestQueue(this).add(request);
+
+    }
+    private void updateRet(String item_id) {
+
+        String item_status = "閒置中";
+        String item_occupybed = "";
+
+        StringRequest request = new StringRequest(Request.Method.POST, urlRetUpdate,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+                            if (success.equals("1")){
+                                Toast.makeText(ReturnActivity.this, "歸還成功", Toast.LENGTH_SHORT).show();
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(ReturnActivity.this, "err" + e.toString(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ReturnActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("item_status", item_status);
+                params.put("item_id", item_id);
+                params.put("item_occupybed", item_occupybed);
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(ReturnActivity.this);
+        requestQueue.add(request);
 
     }
 }
