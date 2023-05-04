c575175b5192d43fe0a887ed83b41ccca6703840
LAPTOP-UF2JJMM9\chiu9 <tyuighjk86@gmail.com>
2023-05-04 15:42:18 +0800

    更新借物功能

--- app/src/main/java/com/example/projectgrade3_two/RentActivity.java
+++ app/src/main/java/com/example/projectgrade3_two/RentActivity.java
@@ -20,8 +20,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,8 +33,9 @@ public class RentActivity extends AppCompatActivity {
     private Activity context=this;
 
     String urlitemid = "https://projectgrade3two.000webhostapp.com/searchItem.php";
-    String urlrent = "https://projectgrade3two.000webhostapp.com/updaterent.php";
+    String urlrent = "https://projectgrade3two.000webhostapp.com/rent.php";
 
+    String urlRepUpdate = "https://projectgrade3two.000webhostapp.com/updateStatus.php";
 
     Button btn_rent_send;
     String item_id, user_id, type;
@@ -44,6 +43,7 @@ public class RentActivity extends AppCompatActivity {
     TextView txtItemID, txtItemName;
 
     EditText txtDate,txtUser, txtDep;
+    RadioGroup Group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,14 +72,11 @@ public class RentActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        RadioGroup radioGroup = findViewById(R.id.radioGroup);
-
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        Group = findViewById(R.id.Group);
+        Group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                switch (i){
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedID) {
+                switch (checkedID){
                     case R.id.radio_lend:
                         type = "lend";
                         break;
@@ -90,10 +87,14 @@ public class RentActivity extends AppCompatActivity {
             }
         });
 
+
+
+
         btn_rent_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 insertRentData();
+                updateRent(item_id);
                 Intent intent = new Intent(RentActivity.this, HomeActivity.class);
                 Bundle bundle = new Bundle();
                 bundle.putString("user_id", user_id);
@@ -160,12 +161,12 @@ public class RentActivity extends AppCompatActivity {
     }
 
     private void insertRentData() {
-
         String rent_user = txtUser.getText().toString();
         String rent_department = txtDep.getText().toString();
         String rent_end = txtDate.getText().toString();
         String rent_item = item_id;
         String rent_type = type;
+
 
         StringRequest request = new StringRequest(Request.Method.POST, urlrent,
                 new Response.Listener<String>() {
@@ -197,13 +198,61 @@ public class RentActivity extends AppCompatActivity {
 
         ){
             @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
+            protected Map<String, String> getParams() throws AuthFailureError{
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("rent_end", rent_end);
                 params.put("rent_user", rent_user);
                 params.put("rent_department", rent_department);
+                params.put("rent_end", rent_end);
                 params.put("rent_item", rent_item);
                 params.put("rent_type", rent_type);
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(RentActivity.this);
+        requestQueue.add(request);
+
+    }
+
+    private void updateRent(String item_id) {
+
+        String item_status = "借用中";
+
+        StringRequest request = new StringRequest(Request.Method.POST, urlRepUpdate,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+                            if (success.equals("1")){
+//                                Toast.makeText(RentActivity.this, "登記成功", Toast.LENGTH_SHORT).show();
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(RentActivity.this, "err" + e.toString(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(RentActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("item_status", item_status);
+                params.put("item_id", item_id);
 
                 return params;
             }
