d4fa44ff67610ee6e9f24cdfd54b1dd4a1008e6d
LAPTOP-UF2JJMM9\chiu9 <tyuighjk86@gmail.com>
2023-05-10 21:24:38 +0800

    點班時設定無須填寫/填寫

--- app/src/main/java/com/example/projectgrade3_two/DoneActivity.java
+++ app/src/main/java/com/example/projectgrade3_two/DoneActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,8 +39,9 @@ public class DoneActivity extends AppCompatActivity {
 
     String urlDoneUpdate = "https://projectgrade3two.000webhostapp.com/checkItem.php";
     String urlitemid = "https://projectgrade3two.000webhostapp.com/searchItem.php";
+    String urlremark = "https://projectgrade3two.000webhostapp.com/remark.php";
 
-    TextView txtItemID, txtItemName;
+    TextView txtItemID, txtItemName, txtNoremark;
 
 
     @Override
@@ -54,6 +58,10 @@ public class DoneActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY); //隱藏手機虛擬按鍵HOME/BACK/LIST按鍵
 
         txtUpDescribe = findViewById(R.id.ed_describe);
+        txtNoremark = findViewById(R.id.txt_Noremark);
+
+        txtNoremark.setVisibility(View.INVISIBLE);
+        txtUpDescribe.setVisibility(View.INVISIBLE);
 
         btn_done = findViewById(R.id.btn_done);
 
@@ -64,6 +72,7 @@ public class DoneActivity extends AppCompatActivity {
 
 
         itemQrcode(item_id);
+        remark(item_id);
 
         btn_done.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -181,6 +190,70 @@ public class DoneActivity extends AppCompatActivity {
 
         RequestQueue requestQueue = Volley.newRequestQueue(DoneActivity.this);
         requestQueue.add(request);
+
+    }
+    private void remark(String item_id){
+        TableLayout.LayoutParams row_layout = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT);
+        TableRow.LayoutParams view_layout = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+        StringRequest request = new StringRequest(Request.Method.POST, urlremark,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("remrkData");
+
+                            if (success.equals("1")) {
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject object = jsonArray.getJSONObject(i);
+
+                                    TableRow trUndo = new TableRow(DoneActivity.this);
+
+                                    trUndo.setLayoutParams(row_layout);
+                                    trUndo.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                                    TextView itemId = new TextView(DoneActivity.this);
+                                    itemId.setText(object.getString("item_id").trim());
+                                    itemId.setLayoutParams(view_layout);
+
+                                    TextView itemName = new TextView(DoneActivity.this);
+                                    itemName.setText(object.getString("item_name").trim());
+                                    itemName.setLayoutParams(view_layout);
+
+                                    txtNoremark.setVisibility(View.VISIBLE);
+
+
+                                }
+
+                            } else if (success.equals("2")) {
+                                txtUpDescribe.setVisibility(View.VISIBLE);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(DoneActivity.this, "err", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(DoneActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("item_id", item_id);
+
+                return params;
+            }
+        };
+
+        Volley.newRequestQueue(this).add(request);
 
     }
 
